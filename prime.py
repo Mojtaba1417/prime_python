7a47727e525b3e0345f92d0575924ab9bf9431e5
Mojtaba Ghahri <mojtaba.1417@yahoo.com>
2023-01-01 11:27:27 +0330

    create prime.pyi to declare notation and modify module

--- prime.py
+++ prime.py
@@ -1,18 +1,23 @@
 # Prime module (Mojtaba Ghahri)
+__all__ =[
+    "Prime","DetectPrime","RangePrimes"
+]
 class Prime:
-    def __init__(self) -> None:
+    def __init__(self):
         self.flag = 0
         self.prime_list = []
 
     def DetectPrime(self, number):
         for i in range(2, number):
+            self.flag = 0
             if number % i == 0:
                 self.flag += 1
                 break
-        if self.flag > 0:
+        if self.flag == 0:
             return True
         else:
             return False
+
 
     def RangePrimes(self, start, stop):
         """accept start point and end point to find prime numbers between them"""
@@ -24,4 +29,11 @@ class Prime:
     def __str__(self) -> str:
         return f'{self.prime_list}' 
 
+_inst = Prime()
+DetectPrime = _inst.DetectPrime
+RangePrimes = _inst.RangePrimes
+
+
+if __name__ == '__main__':
+    print("Wellcome to prime module")
 
--- test.py
+++ test.py
@@ -6,17 +6,22 @@ def findthree(num):
     first = 0
     second = 0
     third = 0
-    a = prime.Prime()
-    s = a.RangePrimes(1,num)
+    s = prime.RangePrimes(1,num)
+    print(s)
     for i in range(1,len(s)):
         first = s[i]
-        for j in range(1,len(s)):
+        for j in range(1, len(s)):
             second = s[j]
             third = num - first - second
-            if third > 0:
+            if third > 0 and prime.DetectPrime(third):
                 combination.append([first, second, third])
+            
     return combination
 
 
-print(findthree(20))
+print(findthree(100))
+
+
+
+
         
